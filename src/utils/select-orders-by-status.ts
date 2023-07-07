988f93760e4e1e639cbc3ab83c24d1125d1c7bbe
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-07 18:35:31 -0300

    refactor: Removido funcao

--- src/utils/select-orders-by-status.ts
+++
@@ -1,6 +0,0 @@
-import { OrdersProps } from '../repository/interfaces/order'
-
-export function selectOrdersByStatus(orders: OrdersProps[], status?: string) {
-  const filteredOrders = orders.filter((order) => order.status === status)
-  return filteredOrders
-}
