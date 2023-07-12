f9856f791dc2c75495e3f5af2831b18054d619bb
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-11 22:08:21 -0300

    refactor: Movido IOrderService para contracts

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -1,17 +1,5 @@
 import { IOrders, OrdersProps } from '../../repository/interfaces/order'
-
-export interface IOrderService {
-  createOrder(user: Object, products: Object[]): Promise<OrdersProps>
-  findOrders(querie?: string): Promise<OrdersProps[] | undefined>
-  findOrderById(orderId: string): Promise<OrdersProps>
-  deleteOrder(orderId: string): Promise<void>
-  updateOrderStatus(orderId: string, status: string): Promise<OrdersProps>
-  updateOrderQuantity(
-    orderId: string,
-    productId: string,
-    quantity: number,
-  ): Promise<OrdersProps>
-}
+import { IOrderService } from '../contracts/iorder.service'
 
 export default class OrderService implements IOrderService {
   private orderRepository: IOrders
